0c96ebc16088c735d4db7fc4e8196c6376f9cd4c
Gustavo Azevedo <gustavoaagamer@gmail.com>
2025-11-25 16:34:18 -0300

    Add IBaseEntity and implement BaseRepository

--- TocaAi.Infrastrucutre/Context/TocaAiDbContext.cs
+++ TocaAi.Infrastrucutre/Context/TocaAiDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace TocaAi.Infrastrucutre.Context
+﻿using Microsoft.EntityFrameworkCore;
+using TocaAi.Domain.Entities;
+
+namespace TocaAi.Infrastrucutre.Context
 {
-    public class TocaAiDbContext
+    public class TocaAiDbContext : DbContext
     {
+        public TocaAiDbContext(DbContextOptions<TocaAiDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
     }
 }
---
+++ TocaAi.Infrastrucutre/Repository/BaseRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TocaAi.Domain.Base;
+using TocaAi.Infrastrucutre.Context;
+
+namespace TocaAi.Infrastrucutre.Repository
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class, IBaseEntity
+    {
+        protected readonly TocaAiDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(TocaAiDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public void AttachObject(object obj)
+        {
+            _context.Attach(obj);
+        }
+
+        public void ClearChangeTracker()
+        {
+            _context.ChangeTracker.Clear();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(object id)
+        {
+            var entity = _dbSet.Find(id);
+
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                _context.SaveChanges();
+            }
+        }
+
+        public IList<TEntity> Select(List<string>? includes = null)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                    query = query.Include(include);
+            }
+
+            return query.ToList();
+        }
+
+        public TEntity? SelectById<TId>(TId id, IList<string>? includes = null)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if(includes != null)
+            {
+                foreach (var include in includes)
+                    query = query.Include(include);
+            }
+
+            return query.FirstOrDefault(e => e.Id.Equals(id));
+        }
+    }
+}
